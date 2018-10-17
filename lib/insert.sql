ce836a4a21ca8b5391c26d4bf9c0b1e4af8c9f6c
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-10-17 17:14:18 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Bear 1",5,"M","Brown","Chill",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Bear 2",6,"M","Brown","Chill",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Bear 3",7,"M","Brown","Chill",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Bear 4",9,"M","Brown","Chill",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Bear 5",10,"M","Brown","Not Chill",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Bear 6",11,"F","Brown","Not Chill",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Bear 7",12,"F","Brown","Not Chill",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (NULL,13,"F","Brown","Not Chill",0);
